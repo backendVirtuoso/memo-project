ba2f6cfd9394483d006d716118dc0cfcada8c6e1
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-01-04 01:36:43 +0900

    최종 코드

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import { useCallback, useState } from 'react';
 import './App.css';
+import MemoContainer from './components/MemoContainer';
+import SideBar from './components/SideBar';
+import { setItem, getItem } from './lib/storage';
+import debounce from 'lodash.debounce';
+
+const debouncedSetItem = debounce(setItem, 5000);
 
 function App() {
+  const [memos, setMemos] = useState(getItem('memo') || []);
+
+  const [selectedMemoIndex, setSelectedMemoIndex] = useState(0);
+
+  const setMemo = useCallback(
+    (newMemo) => {
+      setMemos((memos) => {
+        const newMemos = [...memos];
+
+        newMemos[selectedMemoIndex] = newMemo;
+        debouncedSetItem('memo', newMemos);
+
+        return newMemos;
+      });
+    },
+    [selectedMemoIndex],
+  );
+
+  const addMemo = useCallback(() => {
+    setMemos((memos) => {
+      const now = new Date().getTime();
+      const newMemos = [
+        ...memos,
+        {
+          title: 'Untitled',
+          content: '',
+          createdAt: now,
+          updatedAt: now,
+        },
+      ];
+
+      debouncedSetItem('memo', newMemos);
+
+      return newMemos;
+    });
+    setSelectedMemoIndex(memos.length);
+  }, [memos]);
+
+  const deleteMemo = useCallback(
+    (index) => {
+      setMemos((memos) => {
+        const newMemos = [...memos];
+
+        newMemos.splice(index, 1);
+        debouncedSetItem('memo', newMemos);
+
+        return newMemos;
+      });
+      if (index === selectedMemoIndex) {
+        setSelectedMemoIndex(0);
+      }
+    },
+    [selectedMemoIndex],
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideBar
+        memos={memos}
+        addMemo={addMemo}
+        selectedMemoIndex={selectedMemoIndex}
+        setSelectedMemoIndex={setSelectedMemoIndex}
+        deleteMemo={deleteMemo}
+      />
+      <MemoContainer memo={memos[selectedMemoIndex]} setMemo={setMemo} />
     </div>
   );
 }
